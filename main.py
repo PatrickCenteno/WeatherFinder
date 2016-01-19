ad9e6ccf66e3c0614fc1bda771cceaa20d1d07f8
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-19 11:33:16 -0500

    gets location information based on ip address

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #WeatherFinder
 import util
+import geocoder
+import sys
+
+WEATHER_API = 'api.openweathermap.org/data/2.5/weather?'
 
 def main():
 	if util.is_connected():
 		my_ip = util.get_IP()
 		print my_ip
+		location_values = get_location_values(my_ip)	
 	else:
 		print 'Must be connected to internet.'
+		sys.exit()
+
+def get_location_values(my_ip):
+	g = geocoder.ip(str(my_ip))
+	return [g.lat, g.lng, g.city, g.state, g.country]
+	
 
 if __name__ == '__main__':
 	main()

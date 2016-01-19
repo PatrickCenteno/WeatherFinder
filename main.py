03f70410577e827e67c771a3b28b71915395e83c
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-19 12:07:04 -0500

    Makes api call with api key yet

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import util
 import geocoder
 import sys
+import requests
 
 WEATHER_API = 'api.openweathermap.org/data/2.5/weather?'
 
@@ -9,7 +10,10 @@ def main():
 	if util.is_connected():
 		my_ip = util.get_IP()
 		print my_ip
-		location_values = get_location_values(my_ip)	
+		#[latitude, longitude, city, state, country]
+		location_values = get_location_values(my_ip)
+		weather = get_weather(location_values[0], location_values[1])	
+		print weather
 	else:
 		print 'Must be connected to internet.'
 		sys.exit()
@@ -17,6 +21,12 @@ def main():
 def get_location_values(my_ip):
 	g = geocoder.ip(str(my_ip))
 	return [g.lat, g.lng, g.city, g.state, g.country]
+
+def get_weather(lat, lng):
+	params = {'lat':lat, 'lon':lng}
+	r = requests.get(WEATHER_API, params = params)
+	print (r.url)
+	return r.text
 	
 
 if __name__ == '__main__':

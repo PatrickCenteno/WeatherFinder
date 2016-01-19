b62c3a24acab6e61d07da6bb3c2dcd2eb28afac5
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-19 14:13:03 -0500

    Moved files over to main and made successful api call

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 import requests
 
 REMOTE_SERVER = "www.reddit.com"
-WEATHER_API = 'api.openweathermap.org/data/2.5/weather?'
+WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather?'
 API_KEY = '209bb1808f1fca53362d3704d127f45f'
 
 def main():
@@ -15,10 +15,13 @@ def main():
 		print my_ip
 		#[latitude, longitude, city, state, country]
 		location_values = get_location_values(my_ip)
+		print 'Retrieving weather for Brooklyn, NY'
+		weather = get_weather(location_values[0], location_values[1])
+		
 	else:
 		print 'Must be connected to internet.'
 		sys.exit()
-		
+
 
 def get_location_values(my_ip):
 	g = geocoder.ip(str(my_ip))
@@ -26,10 +29,10 @@ def get_location_values(my_ip):
 
 
 def get_weather(lat, lng):
-	params = {'lat':lat, 'lon':lng}
+	params = {'lat':str(lat), 'lon':str(lng), 'APPID':API_KEY}
 	r = requests.get(WEATHER_API, params = params)
-	print (r.url)
-	return r.text
+	#print (r.url)
+	return r.json
 
 
 def is_connected():
@@ -49,7 +52,10 @@ def get_IP():
 	return urlopen('http://ip.42.pl/raw').read()
 
 
-	
+def to_faren(temp):
+	return (temp - 273.15) * 1.8 + 32
+
+
 
 if __name__ == '__main__':
 	main()

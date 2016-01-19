47bd58c370696e346ea97f449d5f3128da6f0799
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-19 13:48:42 -0500

    Consolidated into one file

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
 #WeatherFinder
-import util
+import socket
+from urllib2 import urlopen
 import geocoder
 import sys
 import requests
 
+REMOTE_SERVER = "www.reddit.com"
 WEATHER_API = 'api.openweathermap.org/data/2.5/weather?'
+API_KEY = '209bb1808f1fca53362d3704d127f45f'
 
 def main():
-	if util.is_connected():
-		my_ip = util.get_IP()
+	if is_connected():
+		my_ip = get_IP()
 		print my_ip
 		#[latitude, longitude, city, state, country]
 		location_values = get_location_values(my_ip)
-		weather = get_weather(location_values[0], location_values[1])	
-		print weather
 	else:
 		print 'Must be connected to internet.'
 		sys.exit()
+		
 
 def get_location_values(my_ip):
 	g = geocoder.ip(str(my_ip))
 	return [g.lat, g.lng, g.city, g.state, g.country]
+
 
 def get_weather(lat, lng):
 	params = {'lat':lat, 'lon':lng}
 	r = requests.get(WEATHER_API, params = params)
 	print (r.url)
 	return r.text
+
+
+def is_connected():
+	try:
+	    # see if we can resolve the host name -- tells us if there is
+	    # a DNS listening
+	    host = socket.gethostbyname(REMOTE_SERVER)
+	    # connect to the host -- tells us if the host is actually
+	    # reachable
+	    s = socket.create_connection((host, 80), 2)
+	    return True
+	except:
+		return False
+
+
+def get_IP():
+	return urlopen('http://ip.42.pl/raw').read()
+
+
 	
 
 if __name__ == '__main__':

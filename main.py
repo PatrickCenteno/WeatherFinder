e4b1601194d8fd59facefccf6494a92384e86965
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-19 09:47:26 -0500

    Testing internet connection and obtaining IP Address

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WeatherFinder
+import util
+
+def main():
+	if util.is_connected():
+		my_ip = util.get_IP()
+		print my_ip
+	else:
+		print 'Must be connected to internet.'
+
+if __name__ == '__main__':
+	main()

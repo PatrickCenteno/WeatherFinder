7557f5418678cb7b8ee71f1a76557a708ac076b7
Patrick Centeno <pcenteno3492@gmail.com>
2016-01-21 13:45:11 -0500

    Cleaned up code a bit and changed Internet connection checking URL to google

--- main.py
+++ main.py
@@ -6,27 +6,22 @@ import sys
 import requests
 import json
 
-REMOTE_SERVER = "www.reddit.com" #:)
+REMOTE_SERVER = "www.google.com" #:)
 WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather?'
 API_KEY = '209bb1808f1fca53362d3704d127f45f'
 
 def main():
 	if is_connected():
-		my_ip = get_IP()
-
+		print 'Loading your weather...'
 		#[latitude, longitude, city, state, country]
-		location_values = get_location_values(my_ip)
-
-		print 'Your IP Address is: ' + my_ip
-		print 'Here\'s some weather information for '+ location_values[2] + \
-			', ' + location_values[3] + ':'
+		location_values = get_location_values(get_IP())
 
 		# Parse the Json response
-		raw_response = get_weather(location_values[0], location_values[1])
-		weather_json = json.loads(raw_response)
+		weather_json = json.loads(get_weather(location_values[0], location_values[1]))
 		temperature = to_faren(int(weather_json['main']['temp']))
-		print 'It is currently ' + str(temperature) + ' degrees farenheit'
 		
+		#print the info to the console
+		display(get_IP(), location_values[2], location_values[3], temperature)
 		
 	else:
 		print 'Must be connected to internet.'
@@ -73,6 +68,11 @@ def get_IP():
 def to_faren(temp):
 	return (temp - 273.15) * 1.8 + 32
 
+def display(ip, city, state, temp):
+	print 'Your IP Address is: ' + ip
+	print 'Here\'s some weather information for '+ city + \
+		', ' + state + ':'
+	print 'It is currently ' + str(temp) + ' degrees farenheit'
 
 
 if __name__ == '__main__':
